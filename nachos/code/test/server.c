bb25e543e5e608c25e499278ad44f13c72c8132e
Ziyang <zchen618@icloud.com>
2018-12-07 15:34:02 -0500

    ver 1.1

--- nachos/code/test/server.c
+++ nachos/code/test/server.c
@@ -5,15 +5,12 @@
 
 int main() {
 	OpenFileId output = ConsoleOutput;
-	char* str = "Prog server: server started \n";
 	char* msg;
 	char *res;
 	char *ans;
 	ThreadYield();
-	Write(str, 30, output);
 	
 	WaitMessage("client", msg, "buffer1");
-	Write(msg, 5, output);
 
 	SendAnswer("permit", "ACK J+1", "buffer1");
 	SendMessage("client", "SYN K", "buffer2");
